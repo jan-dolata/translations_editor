5650e9ffeb0bd7e6258294686f590b2d2e562faa
Jan Dolata <j.dolata@holonglobe.com>
2016-04-18 18:46:10 +0200

    'middleware' => 'auth' in routes

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,7 +3,7 @@
 /**
  * Translations
  */
-Route::group(['prefix' => 'translation'], function () {
+Route::group(['prefix' => 'translation', 'middleware' => 'auth'], function () {
     Route::get('get/{file?}', 'TranslationController@get')->name('translation_get');
     Route::post('save', 'TranslationController@save')->name('translation_save');
     Route::get('log', 'TranslationController@log')->name('translation_log');
